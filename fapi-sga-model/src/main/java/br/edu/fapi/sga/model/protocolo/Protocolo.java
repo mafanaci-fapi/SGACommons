127ac9dd70d26182faaa198d4629775901955447
Guilherme <guilherme1301703@fapi-pinhais.edu.br>
2018-06-27 00:20:01 -0300

    Feito classe de modelo para protocolo.

---
+++ fapi-sga-model/src/main/java/br/edu/fapi/sga/model/protocolo/Protocolo.java
@@ -0,0 +1,23 @@
+package br.edu.fapi.sga.model.protocolo;
+
+public class Protocolo {
+
+    private String descricao;
+    private String tipoProtocolo;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTipoProtocolo() {
+        return tipoProtocolo;
+    }
+
+    public void setTipoProtocolo(String tipoProtocolo) {
+        this.tipoProtocolo = tipoProtocolo;
+    }
+}
