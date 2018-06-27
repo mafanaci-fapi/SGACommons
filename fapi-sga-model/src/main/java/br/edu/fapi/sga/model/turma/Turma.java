53654d9a7c0b12cc502613d1f7217c70f1986670
Jonas Siqueira Marcelino <jonas.1301721@fapi-pinhais.edu.br>
2018-06-26 23:10:58 -0300

    Criando a classe Model-Turma

--- fapi-sga-model/src/main/java/br/edu/fapi/sga/model/turma/Turma.java
+++ fapi-sga-model/src/main/java/br/edu/fapi/sga/model/turma/Turma.java
@@ -2,4 +2,27 @@ package br.edu.fapi.sga.model.turma;
 
 public class Turma {
 
+	
+	private String nome;
+	private int codigo;
+	private int id;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+		
 }
