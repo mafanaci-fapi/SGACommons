02362eea6ccd97b2f190d65bdabbea1f71b91b93
Ronei <ronei.1301719@fapi-pinhais.edu.br>
2018-07-02 22:36:55 -0300

    Retirada do static do idAluno

--- fapi-sga-model/src/main/java/br/edu/fapi/sga/model/aluno/Aluno.java
+++ fapi-sga-model/src/main/java/br/edu/fapi/sga/model/aluno/Aluno.java
@@ -8,55 +8,73 @@ public class Aluno {
 	private String telefone;
 	private String email;
 	private String endereco;
-	private static int idAluno;
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	public String getRg() {
-		return rg;
-	}
-	public void setRg(String rg) {
-		this.rg = rg;
-	}
-	public String getTelefone() {
-		return telefone;
-	}
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getEndereco() {
-		return endereco;
-	}
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	} 
-	public static int getIdAluno() {
-		return idAluno;
-	} 
-	public static void setIdAluno(int idAluno) {
-		Aluno.idAluno = idAluno;
-	}
-	public int getCodigo() {
-		return codigo;
-	}
-	public void setCodigo(int codigo) {
-		this.codigo = codigo;
-	}
-	
-	
+	private int idAluno;
+
+        public int getCodigo() {
+        return codigo;
+        }
+        
+        public void setCodigo(int codigo) {
+        this.codigo = codigo;
+        }
+        
+        public String getNome() {
+        return nome;
+        }
+        
+        public void setNome(String nome) {
+        this.nome = nome;
+        }
+        
+        public String getCpf() {
+        return cpf;
+        }
+
+        public void setCpf(String cpf) {
+        this.cpf = cpf;
+        }
+
+        public String getRg() {
+        return rg;
+        }
+
+        public void setRg(String rg) {
+        this.rg = rg;
+        }
+
+        public String getTelefone() {
+        return telefone;
+        }
+
+        public void setTelefone(String telefone) {
+        this.telefone = telefone;
+        }
+
+        public String getEmail() {
+        return email;
+        }
+
+        public void setEmail(String email) {
+        this.email = email;
+        }
+
+        public String getEndereco() {
+        return endereco;
+        }
+
+        public void setEndereco(String endereco) {
+        this.endereco = endereco;
+        }
+
+        public int getIdAluno() {
+        return idAluno;
+        }
+
+        public void setIdAluno(int idAluno) {
+        this.idAluno = idAluno;
+        }
+
+        public void setId(String nextLine) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
 }
